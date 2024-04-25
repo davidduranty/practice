9fb58908d3c21ac9698b0ab96ad25b4444bbd6b2
David Duranty <dadaduranty@gmail.com>
2024-04-25 12:13:53 +0200

    Next-js Portfolio skills and href

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Header } from "./_components/Header";
 import { Hero } from "./_components/Hero";
 import { Spacing } from "./_components/Spacing";
 import { Status } from "./_components/Status";
+import { Skills } from "./_components/Skills";
 
 export default function Home() {
   return (
@@ -11,7 +12,9 @@ export default function Home() {
       <Spacing size="md"/>
       <Hero />
       <Spacing size="md" />
-      <Status/>
+      <Status />
+      <Spacing size="md" />
+      <Skills/>
    </main>
   );
 }
